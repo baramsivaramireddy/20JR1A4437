fde3ab99a00615ced29aa1287a36dad8df5af168
baramsivaramireddy <baramsivarami@gmail.com>
2023-08-02 12:34:16 +0530

    wrote implementations details

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ const express =  require("express");
 const app = express();
 const PORT = 8000
 
+
+
 app.use((req,res,next) =>{
 
     console.log(`request at ${req.url} at methdo ${req.method} and body ${req.body}`)
     next();
 })
+
+const router = require("./routes/index")
+app.use('/' ,)
 app.listen(PORT , ()=> {
     console.log(`server started at port ${PORT}`)
 })
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,25 @@ const router = requrie("express").Router();
 
 router.get('/' , (req,res) =>{
 
-    res.json({message : "hello world"})
+    res
+        .status(200)
+        .json({message : "testing"})
 }) 
+
+module.exports = router
+
+
+// In these app we have to build a  two  api endpoints
+// 1 end point 
+//         for gettin all trains data and sort them based on results
+
+// 2 end point 
+//         for single train
+
+// we have get data from john server
+
+//  implementation detail
+
+// get access token for every request
+//  when 1 end point got hit . call the john server for all trains
+//  when 2 end point got hit  . call john server for  one train
